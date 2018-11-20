1c9f4d5e5baeba5d743093de2aedc36b25b81224
kalamanek <37038282+kalamanek@users.noreply.github.com>
2018-11-20 02:50:34 +0100

    Add files via upload

---
+++ cos.ps1
@@ -0,0 +1,26 @@
+  # Create temp folder
+   cd C:
+	if(!(Test-Path "C:\GitTemp")){
+		Write-Host "old reposytory didn't exist"
+		mkdir GitTemp
+	}else{
+		
+		Write-Host "deleting old reposytory"
+		Remove-Item -path C:\GitTemp -Force -Recurse
+		mkdir GitTemp
+	}
+	
+	
+   # Clone TFD Git repository
+   git clone https://github.com/kalamanek/kalkulator.git C:\GitTemp
+   Write-Host "fresh repository cloned"
+	
+	cd GitTemp
+	
+	Write-Host "reunning tests"
+	$time = "app_"+(Get-Date -UFormat " %H - %M - %S").ToString()
+	mvn test --log-file $time".txt"
+	 
+	Write-Host "test done"
+	
+	Start-Sleep -s 10
